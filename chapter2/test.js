47a43f4c890c1802c69ac62483e7f25fa975de10
fruechss <fruechss@rose-hulman.edu>
2023-03-09 16:38:46 -0500

    Deleted corrupted astronaut image

---
+++ chapter2/test.js
@@ -0,0 +1,11 @@
+ let x = 5; // Initializing variable (// = comment)
+ /* this is
+a multi-line
+comment */
+function myFunc() {
+    // do something block
+}
+let sum = x + 3; // Use this spacing
+let totalNumber = 3;
+function combineAllNumbers() {
+} // Use camelCase for variable names and function names
